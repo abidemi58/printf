1f5760478a699724e5286a99ceb5cb2e67048e2f
Favour Baruch <78267322+chigo01@users.noreply.github.com>
2022-08-09 12:45:59 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
+
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 int _printf(const char *format, ...);
 int print_char(va_list ap);
 int print_str(va_list ap);
