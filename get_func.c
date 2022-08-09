e463146f99753ccebae2721b5427e0f6686a164e
abidemi58 <abidemiseyi164real@gmail.com>
2022-08-09 05:59:02 -0700

    handler

--- get_func.c
+++ get_func.c
@@ -14,6 +14,15 @@ int (*get_func(char x))(va_list)
 		{"%", print_percent},
 		{"d", print_d},
 		{"i", print_i},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"x", print_hexa_lower},
+		{"X", print_hexa_upper},
+		{"u", print_unsigned},
+		{"S", print_str_unprintable},
+		{"r", print_str_reverse},
+		{"p", print_ptr},
+		{"R", print_rot13},
 		{NULL, NULL}
 	};
 	while (arr[i].valid)
