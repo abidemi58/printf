a199fb1eee2201303c96776e47289bb497010532
Favour Baruch <78267322+chigo01@users.noreply.github.com>
2022-08-09 06:24:50 -0700

    Update functions.c

--- functions.c
+++ functions.c
@@ -134,3 +134,20 @@ int print_nbr(va_list args)
 {
 	return (print_number(va_arg(args, int)));
 }
+
+/**
+ * print_octal - print number octal base
+ * @ap: arg list
+ * Return: number of printed char
+ */
+
+int print_octal(va_list args)
+{
+	char *str;
+	int sum;
+
+	str = convert_base(va_arg(args, unsigned int), 8, 0);
+	sum = _puts(str, 0);
+	free(str);
+	return (sum);
+}
